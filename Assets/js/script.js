94cd2fbcf74676b46607a6c35bc220c949298ee3
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-03-28 21:09:27 -0600

    weather api uses eight 3 hr windows per day, will need to find a way to cycle through the list in increments of 8

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -27,6 +27,7 @@ function getWeather(city) {
   var todayWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=979c8157697c1c8cdda4b522d2ef9ef9`;
   var fiveDayForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=979c8157697c1c8cdda4b522d2ef9ef9`;
 
+  // Pull data and display today's weather
   $.ajax({
     url: todayWeather,
     method: 'GET',
@@ -50,26 +51,44 @@ function getWeather(city) {
     var latitude = response.coord.lat;
     var longitude = response.coord.lon;
     var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=mintutely,hourly,daily,alerts&appid=979c8157697c1c8cdda4b522d2ef9ef9`;
-    
+
     $.ajax({
       url: oneCall,
       method: 'GET'
-  }).then(function (oneCallResults) {
+    }).then(function (oneCallResults) {
       $("#uvText").text("UV Index: ");
       $("#uvIndex").text(oneCallResults.current.uvi);
-      // checks the uv index value to determine whether the severity of UV that day
-      if(oneCallResults.current.uvi <= 2) {
-          $("#uvIndex").attr("class", "low");
-      } else if(oneCallResults.current.uvi <= 5) {
-          $("#uvIndex").attr("class", "moderate");
-      } else if(oneCallResults.current.uvi <= 7) {
-          $("#uvIndex").attr("class", "high");
+      if (oneCallResults.current.uvi <= 2) {
+        $("#uvIndex").attr("class", "low");
+      } else if (oneCallResults.current.uvi <= 5) {
+        $("#uvIndex").attr("class", "moderate");
+      } else if (oneCallResults.current.uvi <= 7) {
+        $("#uvIndex").attr("class", "high");
       } else {
-          $("#uvIndex").attr("class", "veryHigh");
+        $("#uvIndex").attr("class", "veryHigh");
       }
     })
 
   })
+
+  // Pull data and display five day weather outlook
+
+  $.ajax({
+    url: fiveDayForecast,
+    method: 'GET'
+  }).then(function (response) {
+    console.log(response);
+    var allDays = ["day1","day2","day3","day4","day5"]
+    for (i=0; i<=allDays.length; i++) {
+      var dateOutlook = allDays[i];
+      var dateElement = $("<li>");
+      var dateElementText = moment(response.list[i].dt_txt).format('M/D/YYYY');
+      console.log(dateElementText);
+      dateElement.text(dateElementText);
+      console.log(dateElement);
+    }
+  })
+
 }
 
 $(".btn").on("click", pullWeather);
