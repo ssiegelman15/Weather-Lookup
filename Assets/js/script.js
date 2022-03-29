0f7662960c83e4cfef58ab80c88818516cc1c97a
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-03-28 19:37:55 -0600

    testing API call to see structure of results

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,7 @@ var previousCities = [];
 
 
 function pullWeather (event) {
+  // Sequence of events to clear page, store search result, and display weather pulled from Open Weather Map API
   event.preventDefault();
   clearPage();
   var citySearch = $("#cityInput").val();
@@ -10,6 +11,7 @@ function pullWeather (event) {
 }
 
 function storeCity() {
+  // Add search result to global histroy var and store search result locally
   previousCities.push(citySearch);
   localStorage.setItem("History", JSON.stringify(previousCities));
 }
@@ -18,8 +20,16 @@ function clearPage() {
 
 }
 
-function getWeather() {
-
+function getWeather(city) {
+  // Create variables for both current weather & five day Open Weather Map API's
+  var todayWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=979c8157697c1c8cdda4b522d2ef9ef9`;
+  var fiveDayForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=979c8157697c1c8cdda4b522d2ef9ef9`;
+  
+  $.ajax({
+    url: todayWeather,
+    method: 'GET',
+}).then(function (response) {
+  console.log(response);
 }
 
 
