e0c1904e8c3680f0a9b0930d6d53b7d14f6e87ec
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-03-28 20:22:52 -0600

    working on function then realized I need to add location, date and weather icon on first line of results

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,7 @@
 var previousCities = []; 
 
 
-function pullWeather (event) {
+function pullWeather(event) {
   // Sequence of events to clear page, store search result, and display weather pulled from Open Weather Map API
   event.preventDefault();
   clearPage();
@@ -22,12 +22,14 @@ function clearPage() {
 
 function getWeather(city) {
   // Create variables for both current weather & five day Open Weather Map API's
+  var todayDate = moment().format("M/DD/YYYY");
+  $("#cityDisplay").text(city + " " + todayDate);
   var todayWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=979c8157697c1c8cdda4b522d2ef9ef9`;
   var fiveDayForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=979c8157697c1c8cdda4b522d2ef9ef9`;
   var latitude;
   var longitude;
   var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=mintutely,hourly,daily,alerts&appid=979c8157697c1c8cdda4b522d2ef9ef9`;
-
+  
 
   $.ajax({
     url: todayWeather,
@@ -37,10 +39,13 @@ function getWeather(city) {
   var temp = response.main.temp;
   var wind = response.wind.speed;
   var humidity = response.main.humidity;
+  var iconCode = response.weather[0].icon;
+  var weatherIcon = "https://openweathermap.org/img/wn/${iconCode}@2x.png";
+
 
   latitude = response.coord.lat;
   longitude = response.coord.lon;
-  
+
 
 })
 }
