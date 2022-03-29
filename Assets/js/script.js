fb06915b9ad059f39af66c6743561cf62c60f2c9
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-03-28 20:48:41 -0600

    added UV logic to get background set to class dependent on value

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,4 @@
-var previousCities = []; 
+var previousCities = [];
 
 
 function pullWeather(event) {
@@ -26,28 +26,50 @@ function getWeather(city) {
   $("#cityDisplay").text(city + " " + todayDate);
   var todayWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=979c8157697c1c8cdda4b522d2ef9ef9`;
   var fiveDayForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=979c8157697c1c8cdda4b522d2ef9ef9`;
-  var latitude;
-  var longitude;
-  var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=mintutely,hourly,daily,alerts&appid=979c8157697c1c8cdda4b522d2ef9ef9`;
-  
 
   $.ajax({
     url: todayWeather,
     method: 'GET',
-}).then(function (response) {
-  console.log(response);
-  var temp = response.main.temp;
-  var wind = response.wind.speed;
-  var humidity = response.main.humidity;
-  var iconCode = response.weather[0].icon;
-  var weatherIcon = "https://openweathermap.org/img/wn/${iconCode}@2x.png";
+  }).then(function (response) {
+    console.log(response);
 
+    // Add weather icon to first line of results
+    var iconCode = response.weather[0].icon;
+    var weatherIcon = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    var img = $("<img>");
+    img.attr("src", weatherIcon);
+    img.appendTo("#cityDisplay");
 
-  latitude = response.coord.lat;
-  longitude = response.coord.lon;
+    var temp = response.main.temp;
+    var wind = response.wind.speed;
+    var humidity = response.main.humidity;
+    $(".temp").text("Temp: " + temp + String.fromCharCode(176) + "F");
+    $(".wind").text("Wind: " + wind + " MPH");
+    $(".humidity").text("Humditiy: " + humidity + "%");
 
+    var latitude = response.coord.lat;
+    var longitude = response.coord.lon;
+    var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=mintutely,hourly,daily,alerts&appid=979c8157697c1c8cdda4b522d2ef9ef9`;
+    
+    $.ajax({
+      url: oneCall,
+      method: 'GET'
+  }).then(function (oneCallResults) {
+      $("#uvText").text("UV Index: ");
+      $("#uvIndex").text(oneCallResults.current.uvi);
+      // checks the uv index value to determine whether the severity of UV that day
+      if(oneCallResults.current.uvi <= 2) {
+          $("#uvIndex").attr("class", "low");
+      } else if(oneCallResults.current.uvi <= 5) {
+          $("#uvIndex").attr("class", "moderate");
+      } else if(oneCallResults.current.uvi <= 7) {
+          $("#uvIndex").attr("class", "high");
+      } else {
+          $("#uvIndex").attr("class", "veryHigh");
+      }
+    })
 
-})
+  })
 }
 
 $(".btn").on("click", pullWeather);
